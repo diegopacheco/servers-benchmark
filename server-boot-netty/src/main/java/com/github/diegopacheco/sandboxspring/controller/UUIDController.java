b180d619d573c7f4f2ce64f82db9ced4fcc7e0e8
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-19 01:19:57 -0700

    added better reactive code

--- server-boot-netty/src/main/java/com/github/diegopacheco/sandboxspring/controller/UUIDController.java
+++ server-boot-netty/src/main/java/com/github/diegopacheco/sandboxspring/controller/UUIDController.java
@@ -19,5 +19,10 @@ public class UUIDController {
         return Mono.just(UUID.randomUUID().toString());
     }
 
+    @RequestMapping("/r2")
+    public Mono<String> reactive2() {
+        return Mono.fromCallable( ()-> UUID.randomUUID().toString() );
+    }
+
 }
 
