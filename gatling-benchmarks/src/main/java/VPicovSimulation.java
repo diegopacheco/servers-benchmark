9bc017744d8834df62d46f6fb535e7b7aa1d1452
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-20 00:44:44 -0700

    added v and pico but not 100%

--- gatling-benchmarks/src/main/java/VPicovSimulation.java
+++ gatling-benchmarks/src/main/java/VPicovSimulation.java
@@ -10,11 +10,11 @@ public class VPicovSimulation extends Simulation {
 
     HttpProtocolBuilder httpProtocol = http
             .baseUrl("http://localhost:8080")
-            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .acceptHeader("text/html")
             .doNotTrackHeader("1")
             .acceptLanguageHeader("en-US,en;q=0.5")
             .acceptEncodingHeader("gzip, deflate")
-            .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
+            .userAgentHeader("Mozilla/5.0)");
 
     ScenarioBuilder scn = scenario("VPicovSimulation")
             .exec(http("request")
