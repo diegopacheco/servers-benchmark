a384bba2db483d0042252fbf350805e884367746
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-23 03:03:09 -0700

    smart boot http

--- java-smart-http/run.sh
+++ java-smart-http/run.sh
@@ -1,4 +1,8 @@
-
 #!/bin/bash
-./mvnw exec:java -Dexec.mainClass="Main" -Dexec.classpathScope=runtime
 
+ulimit -n 65000
+sudo sysctl -w net.ipv4.ip_local_port_range="1025 65535"
+echo 300000 | sudo tee /proc/sys/fs/nr_open
+echo 300000 | sudo tee /proc/sys/fs/file-max
+
+./mvnw exec:java -Dexec.mainClass="Main" -Dexec.classpathScope=runtime
--- java-smart-http/src/main/java/Main.java
+++ java-smart-http/src/main/java/Main.java
@@ -1,6 +1,23 @@
+import org.smartboot.http.server.HttpBootstrap;
+import org.smartboot.http.server.HttpRequest;
+import org.smartboot.http.server.HttpResponse;
+import org.smartboot.http.server.HttpServerHandler;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class Main{
   public static void main(String args[]){
-    System.out.println("It works!");
+    HttpBootstrap bootstrap = new HttpBootstrap();
+        bootstrap.configuration().debug(false);
+        bootstrap.configuration().threadNum(Runtime.getRuntime().availableProcessors());
+        bootstrap.configuration().readBufferSize(48);
+
+        bootstrap.httpHandler(new HttpServerHandler() {
+            @Override
+            public void handle(HttpRequest request, HttpResponse response) throws IOException {
+                response.write(UUID.randomUUID().toString().getBytes());
+            }
+        }).setPort(8080).start();
   }
 }
