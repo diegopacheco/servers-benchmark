dec8db300d46cf231b37caa6d0d9d3ed11520333
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-24 23:14:32 -0700

    Added elixir working

--- gatling-benchmarks/run-elixir-phoenix-cowboy.sh
+++ gatling-benchmarks/run-elixir-phoenix-cowboy.sh
@@ -5,4 +5,4 @@ sudo sysctl -w net.ipv4.ip_local_port_range="1025 65535"
 echo 300000 | sudo tee /proc/sys/fs/nr_open
 echo 300000 | sudo tee /proc/sys/fs/file-max
 
-./mvnw gatling:test -Dgatling.simulation=ElixirCowboySimulation
+./mvnw gatling:test -Dgatling.simulation=ElixirPhoenixCowboySimulation
